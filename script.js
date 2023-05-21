bc6580a05c15490d1b27d7fbd617fabdd264dff6
denisskka44 <denis.yarovskij@yandex.by>
2023-05-21 18:07:22 +0300

    Project

--- script.js
+++ script.js
@@ -64,3 +64,99 @@ modalController({
   btnOpen: '.section__button5',
   btnClose: '.modal__close'
 });
+
+modalController({
+  modal: '.modal7',
+  btnOpen: '.section__button7',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal8',
+  btnOpen: '.section__button8',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal9',
+  btnOpen: '.section__button9',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal10',
+  btnOpen: '.section__button10',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal11',
+  btnOpen: '.section__button11',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal12',
+  btnOpen: '.section__button12',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal13',
+  btnOpen: '.section__button13',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal14',
+  btnOpen: '.section__button14',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal15',
+  btnOpen: '.section__button15',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal16',
+  btnOpen: '.section__button16',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal17',
+  btnOpen: '.section__button17',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal18',
+  btnOpen: '.section__button18',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal19',
+  btnOpen: '.section__button19',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal20',
+  btnOpen: '.section__button20',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal21',
+  btnOpen: '.section__button21',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal22',
+  btnOpen: '.section__button22',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal23',
+  btnOpen: '.section__button23',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal24',
+  btnOpen: '.section__button24',
+  btnClose: '.modal__close'
+});
+modalController({
+  modal: '.modal25',
+  btnOpen: '.section__button25',
+  btnClose: '.modal__close'
+});
